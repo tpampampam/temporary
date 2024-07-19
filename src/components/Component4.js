95038acdd39534b38a166a15dcf205b6d7ceb1bd
Evgeny <example@gmail.com>
2024-07-19 08:36:09 +0300

    updated COmponent4

--- src/components/Component4.js
+++ src/components/Component4.js
@@ -2,5 +2,12 @@ export const Component4 = () => {
   const ww = () => {
     return 'Privet';
   };
+  const w3w = () => {
+    return 'Privet';
+  };
+  const w2w = () => {
+    return 'Privet';
+  };
+
   return <h1>Privet</h1>;
 };
