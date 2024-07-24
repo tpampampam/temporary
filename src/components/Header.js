906f17895936ba47726e6495218f6cb881a203e6
Evgeny <example@gmail.com>
2024-07-24 08:41:44 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,12 @@ const Header = () => {
     setCount(count => count + num);
   };
   return (
-    <>
+    <div>
+      vvvv
       <h2>{count}</h2>
       <button onClick={() => changeCount(-1)}>decrement</button>
       <button onClick={() => changeCount(1)}>increment</button>
       <div>Header</div>
-    </>
+    </div>
   );
 };
