9e57452a0015913275c2a43db180f8efc724cc0a
Evgeny <example@gmail.com>
2024-07-24 08:37:22 +0300

    new branch

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const Header = () => {
+  const [count, setCount] = useState(0);
+
+  const changeCount = num => {
+    setCount(count => count + num);
+  };
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={() => changeCount(-1)}>decrement</button>
+      <button onClick={() => changeCount(1)}>increment</button>
+      <div>Header</div>
+    </>
+  );
+};
