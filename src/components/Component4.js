789fba917bdef4259cb4a825b9c9731396e0d993
Evgeny <example@gmail.com>
2024-07-24 08:56:18 +0300

    branch br-2

--- src/components/Component4.js
+++ src/components/Component4.js
@@ -2,12 +2,6 @@ export const Component4 = () => {
   const ww = () => {
     return 'Privet';
   };
-  const w3w = () => {
-    return 'Privet';
-  };
-  const wowowowowowo = () => {
-    return 'Privet';
-  };
 
   return <h1>Privet</h1>;
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,6 @@ const Header = () => {
   };
   return (
     <div>
-      vvvv
-      <h2>{count}</h2>
       <button onClick={() => changeCount(-1)}>decrement</button>
       <button onClick={() => changeCount(1)}>increment</button>
       <div>Header</div>
